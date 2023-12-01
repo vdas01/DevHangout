7e4725b0355465a22a94dd1ff9474d670f55888a
Ashmith Kumar Dhawan <ashmith.kumardhawan@gmail.com>
2023-12-01 14:43:47 +0530

    created entities-answer and badge

---
+++ src/main/java/com/springboot/stackoverflow/entity/Answer.java
@@ -0,0 +1,52 @@
+package com.springboot.stackoverflow.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+    @Column(name = "content")
+    private String content;
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    public Answer(){
+    }
+    public Answer(String content, Question question, User user) {
+        this.content = content;
+        this.question = question;
+        this.user = user;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public Question getQuestion() {
+        return question;
+    }
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
