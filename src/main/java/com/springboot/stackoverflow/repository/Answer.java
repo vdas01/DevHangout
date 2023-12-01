968efb4798284ec24555397c28ee150a93a8df7f
Tarun Joshi <tarun.joshi.26.1@mountblue.tech>
2023-12-01 23:34:13 +0530

    duplicate file removed

--- src/main/java/com/springboot/stackoverflow/repository/Answer.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.stackoverflow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Answer extends JpaRepository<Answer,Integer> {
-}
