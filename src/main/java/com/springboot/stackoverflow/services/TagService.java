2c6da3bb41b8174fc77e246c835f157bffb06e29
Vishal Das <vdas53073@gmail.com>
2023-12-03 12:50:06 +0530

    tags controller and homepage and pagination and sorting feature added

--- src/main/java/com/springboot/stackoverflow/services/TagService.java
+++ src/main/java/com/springboot/stackoverflow/services/TagService.java
@@ -1,9 +1,12 @@
 package com.springboot.stackoverflow.services;
 
 import com.springboot.stackoverflow.entity.Tag;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface TagService {
     List<Tag> findAllTags();
+
+    Page<Tag> customTags(Integer pageNo, Integer pageSize, String sortField, String sortDir, String search);
 }
