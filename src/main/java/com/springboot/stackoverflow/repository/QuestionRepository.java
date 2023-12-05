353a16e9e5a1189812ba2a13def1a3a55addd356
Gaurav11raja <gauravpatidar311@gmail.com>
2023-12-06 02:24:38 +0530

    search functionality added

--- src/main/java/com/springboot/stackoverflow/repository/QuestionRepository.java
+++ src/main/java/com/springboot/stackoverflow/repository/QuestionRepository.java
@@ -2,8 +2,15 @@ package com.springboot.stackoverflow.repository;
 
 import com.springboot.stackoverflow.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question,Integer> {
+    @Query("SELECT q FROM Question q WHERE " + "LOWER(q.title) LIKE LOWER(CONCAT('%', :search, '%')) OR "+
+            "LOWER(q.content) LIKE LOWER(CONCAT('%', :search, '%')) OR " +"LOWER(q.author) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+            "EXISTS (SELECT t FROM q.tags t WHERE LOWER(t.name) LIKE LOWER(CONCAT('%', :search, '%')))")
+    List<Question> searchQuestions(String search);
 }
